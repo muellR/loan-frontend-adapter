b98980f98e4b6de09a9c1526daa5538a4ee71ffd
muellR <didimueller@gmx.ch>
2025-08-09 13:00:52 +0200

    add LoanRestController

---
+++ src/main/java/com/appletree/lfa/service/LoanRestController.java
@@ -0,0 +1,23 @@
+package com.appletree.lfa.service;
+
+import com.appletree.lfa.model.Loan;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class LoanRestController implements com.appletree.lfa.api.ServiceApiDelegate {
+
+    public ResponseEntity<List<Loan>> serviceV1LoansByUserUserIdGet(String userId) {
+        try {
+            List<Loan> loans = new ArrayList<>();
+            return ResponseEntity.ok(loans);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(null);
+        }
+    }
+}
