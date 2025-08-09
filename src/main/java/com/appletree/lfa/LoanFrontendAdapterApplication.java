9ad0cb36433042a3e1e01d4947df74568dd6661a
muellR <didimueller@gmx.ch>
2025-08-09 12:36:52 +0200

    first commit (plain spring initializr project)

---
+++ src/main/java/com/appletree/lfa/LoanFrontendAdapterApplication.java
@@ -0,0 +1,13 @@
+package com.appletree.lfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanFrontendAdapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanFrontendAdapterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appletree/lfa/LoanFrontendAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appletree.lfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanFrontendAdapterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
